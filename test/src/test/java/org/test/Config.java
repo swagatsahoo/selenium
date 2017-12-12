1b2fbd51b0d0fe0fa09c5d2de040be9010bb0089
ssahoo <ssahoo@QINDW014.it.local>
2017-12-12 11:10:33 +0200

    No Changes - Initial Config

--- test/src/test/java/org/test/Config.java
+++ test/src/test/java/org/test/Config.java
@@ -1,21 +1,38 @@
 package org.test;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+
 
 public class Config {
 	
-	public WebDriver driver;
+	public static WebDriver driver;
 	
+	public static void Reports() {
+		
+	}
+
 	@BeforeTest
-	public static void Initialization() {
-		System.out.println("Test Success");
-		System.out.println("Test Success 2");
+	public static void Initialization(String url) {
+		System.out.println("Initializing the Browser to start");
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(url);
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public static void MainTest() {
+		System.out.println("Main test");
 	}
 	
 	@AfterTest
 	public static void TearDown() {
-		
+		System.out.println("Finishing Test with Final Touch");
+		Reports();	
+		System.out.println("Script executed and Browser Closed");
 	}
 
 }
