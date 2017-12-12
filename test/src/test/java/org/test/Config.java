fcfc5facfcad1bd4d9a32636781e7d1cb68761b8
ssahoo <ssahoo@QINDW014.it.local>
2017-12-12 12:43:39 +0200

    No Changes - Initial Config

--- test/src/test/java/org/test/Config.java
+++ test/src/test/java/org/test/Config.java
@@ -10,15 +10,15 @@ public class Config {
 	public static WebDriver driver;
 	
 	public static void Reports() {
-		
+		System.out.println("Report section");
 	}
 
 	@BeforeTest
-	public static void Initialization(String url) {
+	public static void Initialization() {
 		System.out.println("Initializing the Browser to start");
 		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get(url);
+		driver.get("https://www.google.co.in");
 		driver.manage().window().maximize();
 		
 	}
@@ -31,7 +31,8 @@ public class Config {
 	@AfterTest
 	public static void TearDown() {
 		System.out.println("Finishing Test with Final Touch");
-		Reports();	
+		Reports();
+		driver.quit();
 		System.out.println("Script executed and Browser Closed");
 	}
 
