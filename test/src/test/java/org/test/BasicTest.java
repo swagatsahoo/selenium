8c388b19696d56b83f4e81f559ea0cf348f4f36c
ssahoo <ssahoo@QINDW014.it.local>
2017-12-12 08:02:59 +0200

    No Changes - Initial Config

--- test/src/test/java/org/test/BasicTest.java
+++ test/src/test/java/org/test/BasicTest.java
@@ -1,7 +1,10 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class BasicTest {
 	
+	@Test
 	public void FirstMethod() {
 		System.out.println("Test Success");
 		System.out.println("Test Success 2");
