5c800c3383a2a74238d540fe9914944a668b1bd3
ssahoo <ssahoo@QINDW014.it.local>
2018-01-19 15:24:01 +0200

    Added Excel Reader

--- test/src/test/java/org/test/utility/ExcelReader.java
+++ test/src/test/java/org/test/utility/ExcelReader.java
@@ -1,9 +1,21 @@
 package org.test.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelReader {
 	
-	public void excelDataFetcher() {
+	public void excelDataFetcher(String excelFileLocation) throws IOException {
 		
+		File src = new File(excelFileLocation);
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheetAt(0);
 	}
 
 }
